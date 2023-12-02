89879ecfa861fa7f0f8a8c5522e9af66f27b9876
TM\super <johnjohn200420042004@yahoo.com.hk>
2023-12-03 06:56:48 +0800

    same as above

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import DataTable from "./DataTable";
 
 function Imageupload(){
     const [picture, setPicture] = useState('');
-    const [name, setName] = useState('');
+    const [jsonData, setJsonData] = useState(null);
+    const [modelName, setModelName] = useState('');
     function handleImage(e){
       console.log(e.target.files)
       setPicture(e.target.files[0])
     }
-    function handleApi(){
+    async function handleApi(){
       const formData = new FormData()
       formData.append('picture', picture)
-      axios.post('http://localhost:8000/api/n_profiles/', formData).then((res) => {
+      await axios.post('http://localhost:8000/api/n_profiles/', formData).then((res) => {
+        console.log(res.data) 
         const extractedModel = res.data.model_name;
-        setName(extractedModel);
-        console.log(res);
+        setModelName(res.data.make_name + " " + res.data.model_name)
         return axios.get(`http://localhost:8000/api/cars/?model=${extractedModel}`);
-      }).then((secondApiResponse) => {
-        console.log('Second API Response:', secondApiResponse);
+      }).then(
+        response => typeof response.data ==='string'? JSON.parse(response.data): response.data).then(data => { 
+        setJsonData(data);
       }).catch((error) => console.error('Error fetching data:', error));
     }
 
@@ -26,7 +29,9 @@ function Imageupload(){
       <div>
         <input type="file" name='file' onChange ={handleImage}/>
         <button onClick= {handleApi}>Submit</button>
-        <div>{name}</div>
+
+        {modelName && <h3>It is a {modelName}</h3>}
+        <DataTable  jsonData={jsonData}/>
       </div>
 
     
