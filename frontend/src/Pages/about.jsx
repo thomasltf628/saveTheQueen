ac2e846882d57ee7df2824abaa066c1e74fd5b31
shavez94 <58049360+shavez94@users.noreply.github.com>
2023-12-14 00:46:33 -0500

    latest 5

--- frontend/src/Pages/about.jsx
+++ frontend/src/Pages/about.jsx
@@ -5,7 +5,16 @@ export const About = () => {
   return (
     <div className='about'>
       <div className="about-contaier">
-      <h1>Team Members</h1>
+      <h1>Overview</h1>
+      <p>To create an application that help users to identify desired product by photos, and then get a best deal through price comparison between different e-commerce platforms.</p>
+      <h1>Team Member</h1>
+      <div className="teamname">
+        <li>TSZ FUNG LIU     100905668</li>
+        <li>SHAVEZ AHMAD AZMI   100938606</li>
+        <li>THOMAS RANGA    100931705</li>
+        <li>REHAN AHMED     100889797</li>
+        <li>ABIODUN ADENIYI NURUDEEN      100915263 </li>
+      </div>
       </div>
     </div>
   )
